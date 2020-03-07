eeedc19e11a2f0535ee27671b3e79124752be91b
Glitch (bills-pc) <none>
2020-03-07 22:05:16 +0000

    🍙👖 Updated with Glitch

--- commands/Info/botinfo.js
+++ commands/Info/botinfo.js
@@ -9,8 +9,7 @@ module.exports ={
     let embed = new Discord.RichEmbed();
     embed.setTitle(`Billy's PC`);
     embed.setDescription(`
-This is Billy's PC. YES, I know it's supposed to be "Bill's PC", 
-I would change it, but I am too lazy, and like being different.
+This is Bill's PC. 
 This is a Pokemon Showdown Tools Bot. Allowing you to backup your teams to your very own 'PC',
 And has other neat tools to help you in the world of Pokemon. ^-^.
 `);
